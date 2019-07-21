8050946671d0248e3d76a12679e7fbaf13146a04
Abhinayaa <abhinayaa1787@gmail.com>
2019-07-20 20:44:32 -0400

    styles added

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#notes").modal("hide");
+  $(".notes").modal("hide");
     $(".saveBtn").bind("click",function(){
 var link=$(this).parent("div").find("a").attr("href");
 console.log(link);
@@ -25,70 +25,70 @@ description:description    }
       // Empty the notes section
     });
     })
-    $(".saveNote").bind("click",function(){
-        // Empty the notes from the note section
-        // $("#notes").empty();
-        // Save the id from the p tag
-        $("#notes").show();
+    // $(".saveNote").on("click",function(){
+    //     // Empty the notes from the note section
+    //     // $("#notes").empty();
+    //     // Save the id from the p tag
+    //     $(".notes").show();
 
-        var thisId = $(this).parent("div").find("a").attr("data-id");
-      console.log(thisId);
-        // Now make an ajax call for the Article
-        $.ajax({
-          method: "GET",
-          url: "/articles/" + thisId
-        })
-          // With that done, add the note information to the page
-          .then(function(data) {
-            console.log(data);
-            // The title of the article
-            $("#notes").append("<h2>" + data.title + "</h2>");
-            // An input to enter a new title
-            $("#notes").append("<input id='titleinput' name='title' >");
-            // A textarea to add a new note body
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-            // A button to submit a new note, with the id of the article saved to it
-            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+    //     var thisId = $(this).parent("div").find("a").attr("data-id");
+    //   console.log(thisId);
+    //     // Now make an ajax call for the Article
+    //     $.ajax({
+    //       method: "GET",
+    //       url: "/saved/" + thisId
+    //     })
+    //       // With that done, add the note information to the page
+    //       .then(function(data) {
+    //         console.log(data);
+    //         // The title of the article
+    //         $(".modal-body").append("<h2>" + data.title + "</h2>");
+    //         // An input to enter a new title
+    //         $(".modal-body").append("<input class='titleinput' name='title' >");
+    //         // A textarea to add a new note body
+    //         $(".modal-body").append("<textarea class='bodyinput' name='body'></textarea>");
+    //         // A button to submit a new note, with the id of the article saved to it
+    //         $(".modal-footer").append("<button data-id='" + data._id + "' clas='savenote'>Save Note</button>");
       
-            // If there's a note in the article
-            if (data.note) {
-              // Place the title of the note in the title input
-              $("#titleinput").val(data.note.title);
-              // Place the body of the note in the body textarea
-              $("#bodyinput").val(data.note.body);
-            }
-          });
-      });
+    //         // If there's a note in the article
+    //         if (data.note) {
+    //           // Place the title of the note in the title input
+    //           $(".titleinput").val(data.note.title);
+    //           // Place the body of the note in the body textarea
+    //           $(".bodyinput").val(data.note.body);
+    //         }
+    //       });
+    //   });
       
       
-    $(".saveNoteBtn").bind("click",function(){
-      var thisId = $(this).parent("div").find("a").attr("data-id");
-
-        // Run a POST request to change the note, using what's entered in the inputs
-        $.ajax({
-          method: "POST",
-          url: "/articles/" + thisId,
-          data: {
-            // Value taken from title input
-            title: $("#titleinput").val(),
-            // Value taken from note textarea
-            body: $("#bodyinput").val()
-          }
-        })
-          // With that done
-          .then(function(data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-            // $("#notes").empty();
-          });
+    // $(".savenote").bind("click",function(){
+    //   var thisId = $(this).attr("data-id");
+    //     // Run a POST request to change the note, using what's entered in the inputs
+    //     $.ajax({
+    //       method: "POST",
+    //       url: "/saved/" + thisId,
+    //       data: {
+    //         // Value taken from title input
+    //         title: $(".titleinput").val(),
+    //         // Value taken from note textarea
+    //         body: $(".bodyinput").val()
+    //       }
+    //     })
+    //       // With that done
+    //       .then(function(data) {
+    //         // Log the response
+    //         console.log(data);
+    //         // Empty the notes section
+    //         // $("#notes").empty();
+    //       });
       
-        // Also, remove the values entered in the input and textarea for note entry
-        $("#titleinput").val("");
-        $("#bodyinput").val("");
-      });
+    //     // Also, remove the values entered in the input and textarea for note entry
+    //     $(".titleinput").val("");
+    //     $(".bodyinput").val("");
+    //   });
       
 $(".deleteArticleBtn").bind("click",function(){
+
     var thisId = $(this).parent("div").find("a").attr("data-id");
     var article=$(this).parent("div").parent("div"); 
     console.log(article);
--- server.js
+++ server.js
@@ -86,40 +86,42 @@ mongoose.connect(MONGODB_URI);
         console.log(err);
       });
   });
-  app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    console.log(req.params.id)
-    db.Article.findOne({_id:req.params.id})     // ..and populate all of the notes associated with it
-      // .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
+  // app.get("/saved/:id", function(req, res) {
+  //   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  //   console.log(req.params.id)
+  //   db.Saved.findOne({_id:req.params.id})     // ..and populate all of the notes associated with it
+  //     // .populate("note")
+  //     .then(function(dbSaved) {
+  //       // If we were able to successfully find an Article with the given id, send it back to the client
 
-        res.json(dbArticle);
-        console.log(dbArticle)
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
-  app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+  //       res.json(dbSaved);
+  //       console.log(dbSaved)
+  //     })
+  //     .catch(function(err) {
+  //       // If an error occurred, send it to the client
+  //       res.json(err);
+  //     });
+  // });
+  // app.post("/saved/:id", function(req, res) {
+  //   console.log(req.body);
+
+  //   // Create a new note and pass the req.body to the entry
+  //   db.Note.create(req.body)
+  //     .then(function(dbNote) {
+  //       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+  //       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+  //       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+  //       return db.Saved.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+  //     })
+  //     .then(function(dbSaved) {
+  //       // If we were able to successfully update an Article, send it back to the client
+  //       res.json(dbSaved);
+  //     })
+  //     .catch(function(err) {
+  //       // If an error occurred, send it to the client
+  //       res.json(err);
+  //     });
+  // });
   
   
   
@@ -148,7 +150,6 @@ app.get("/saved",function(req,res){
   
     })
     app.get("/saved/:id", function(req, res) {
-      console.log(req.params.id)
       db.Saved.deleteOne({ _id: req.params.id })
         .catch(function(err) {
           // If an error occurred, send it to the client
